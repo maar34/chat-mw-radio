646b11bf44aac8cc87599b713fa076083b15d660
maar34 <mar@headbrothers.com>
2023-08-21 15:53:58 +0200

    Update Radio Tree

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const chatForm = document.getElementById('chat-form');
 const chatMessages = document.querySelector('.chat-messages');
-const roomName = document.getElementById('room-name');
+const roomName = "MW-RADIO";
 const userList = document.getElementById('users');
 
 
